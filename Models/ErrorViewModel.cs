aab606435960630163c46834da2b679dc81f26d0
pobe22 <benjaminpollak22@gmail.com>
2025-09-15 11:36:32 +0200

    Remove old files after restructure

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StreamingZeiger.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
