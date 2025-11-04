cba0b4e3f75f4f93667254a814fd4c5334efeb70
pobe22 <benjaminpollak22@gmail.com>
2025-11-04 15:26:39 +0100

    add back api module

--- StreamingZeiger.AppHost/AppHost.cs
+++ StreamingZeiger.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Streamingzeiger_Api>("streamingzeiger-api");
 
+builder.AddProject<Projects.StreamingZeiger_API>("streamingzeiger-api");
+
 builder.Build().Run();
