d8e855df65e23cf55c8875461bce53a76f9a7db2
pobe22 <benjaminpollak22@gmail.com>
2025-11-04 15:31:30 +0100

    fix naming

--- StreamingZeiger.AppHost/AppHost.cs
+++ StreamingZeiger.AppHost/AppHost.cs
@@ -1,7 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Streamingzeiger_Api>("streamingzeiger-api");
-
 builder.AddProject<Projects.StreamingZeiger_API>("streamingzeiger-api");
 
 builder.Build().Run();
